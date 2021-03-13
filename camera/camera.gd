8586cdbd145fc7fff84a678a54466c061f4e6edc
bram <bramreth@gmail.com>
2021-03-13 16:03:50 +0000

    cam hud basics

--- camera/camera.gd
+++ camera/camera.gd
@@ -1,5 +1,6 @@
 extends Spatial
 
+var spread = 1.0 
 
 func add_trauma(trauma_in: int):
 	$juicy_cam.add_trauma(trauma_in)
@@ -16,3 +17,37 @@ func _ready():
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		fade_out()
+	if Input.is_action_just_pressed("click"):
+#		$juicy_cam/CanvasLayer/CenterContainer/anchor/crosshair/AnimationPlayer.play("hit")
+		spread = spread + 30.0
+#		$juicy_cam/CanvasLayer/CenterContainer/anchor/cross.rotation_degrees += 22
+		spread(spread)
+		
+		
+
+func _physics_process(delta):
+	spread = lerp(spread, 1.0, delta * 2)
+	var rot = $juicy_cam/CanvasLayer/CenterContainer/anchor/cross.rotation_degrees
+		
+	
+#	var targ = (1 + floor($juicy_cam/CanvasLayer/CenterContainer/anchor/cross.rotation_degrees/ 90)) * 90
+#
+#	$juicy_cam/CanvasLayer/CenterContainer/anchor/cross.rotation_degrees = lerp(
+#		$juicy_cam/CanvasLayer/CenterContainer/anchor/cross.rotation_degrees,
+#		targ,
+#		delta * 2
+#	)
+	spread(spread)
+	
+func spread(radius: float):
+	var l = $juicy_cam/CanvasLayer/CenterContainer/anchor/cross/l
+	var u = $juicy_cam/CanvasLayer/CenterContainer/anchor/cross/u
+	var r = $juicy_cam/CanvasLayer/CenterContainer/anchor/cross/r
+	var d = $juicy_cam/CanvasLayer/CenterContainer/anchor/cross/d
+	l.position.x = -radius
+	u.position.y = -radius
+	r.position.x = radius
+	d.position.y = radius
+#	cross.scale = Vector2(radius, radius)
+	pass	
+	
