bbea87309838f1d55a10df7d1ae231f14787834a
bram <bramreth@gmail.com>
2021-03-17 14:52:34 +0000

    more ammo unlock stuff

--- scenes/prefabs/hud.gd
+++ scenes/prefabs/hud.gd
@@ -15,6 +15,15 @@ func update_health(health, armor) -> void:
 func add_ammo(ammo_dat):
 	$new_ammo/Panel/VBoxContainer/ammo_ico.texture = ammo_dat.icon
 	$new_ammo/Panel/VBoxContainer/ammo_name.text = ammo_dat.name
+	match(ammo_dat.rarity):
+		ammo_dat.rarities.COMMON:
+			$new_ammo/Panel/VBoxContainer/ammo_name.set("custom_colors/font_color", Color.whitesmoke)
+		ammo_dat.rarities.UNCOMMON:
+			$new_ammo/Panel/VBoxContainer/ammo_name.set("custom_colors/font_color", Color.deepskyblue)
+		ammo_dat.rarities.RARE:
+			$new_ammo/Panel/VBoxContainer/ammo_name.set("custom_colors/font_color", Color.yellow)
+		ammo_dat.rarities.EPIC:
+			$new_ammo/Panel/VBoxContainer/ammo_name.set("custom_colors/font_color", Color.fuchsia)
 	$new_ammo/AnimationPlayer.play("show")
 	$new_ammo/Timer.start()
 	
