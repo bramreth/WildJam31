580e0c9a6f275b65bafb5682a6ecc29c5c2bbfcf
Sam Le-Cornu <slecornu96@gmail.com>
2021-03-19 17:42:19 +0000

    Doors that close

--- globals/Event.gd
+++ globals/Event.gd
@@ -3,6 +3,7 @@ extends Node
 const GAME_LOADED = "game_loaded"
 const GAME_INITIALISED = "game_initialised"
 const OPEN_DOORS = "open_doors"
+const CLOSE_DOORS = "close_doors"
 
 signal game_loaded(player_data)
 signal game_initialised()
--- scenes/prefabs/door.gd
+++ scenes/prefabs/door.gd
@@ -1,8 +1,16 @@
 extends Spatial
 
-
+var open:bool = false
 func _ready():
 	Event.connect_signal(Event.OPEN_DOORS, self, "_on_open")
+	Event.connect_signal(Event.CLOSE_DOORS, self, "_on_close")
 
 func _on_open():
-	$animation_player.play("open_doors")
+	if not open:
+		$animation_player.play("open_doors")
+		open = true
+
+func _on_close():
+	if open:
+		$animation_player.play("close_doors")
+		open = false
