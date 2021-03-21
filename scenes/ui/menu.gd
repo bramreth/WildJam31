5fd661016e4a78a808d605617237a043b2f7ee68
Sam Le-Cornu <slecornu96@gmail.com>
2021-03-21 13:26:23 +0000

    main menu updated

--- scenes/ui/menu.gd
+++ scenes/ui/menu.gd
@@ -1,8 +1,38 @@
 extends Control
 
+onready var contents = $contents
+onready var game_modes = $contents/game_modes
+onready var settings = $contents/settings
+onready var credits = $contents/credits
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	Game.play_background_audio(Game.AUDIO_TRACK_DEFAULT)
 
-func _on_play_game_button_pressed():
-	Scene.change(Scene.PLAYGROUND)
+func _on_single_player_on_pressed():
+	$AnimationPlayer.play("dip_to_black")
+	
+func _on_Options_on_pressed():
+	_show(settings)
+
+func _on_Credits_on_pressed():
+	_show(credits)
+
+func _on_Game_Mode_on_pressed():
+	_show(game_modes)
+
+func _on_Quit_on_pressed():
+	get_tree().exit()
+
+func _show(node:Control) -> void:
+	for view in contents.get_children():
+		if view != node:
+			view.hide()
+		else:
+			view.show()
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "dip_to_black":
+		Scene.change(Scene.SAMCADE)
+
+func _on_social_meta_clicked(meta):
+	OS.shell_open(meta)
