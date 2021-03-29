eac86b4342382726f62a859408aca32437f27648
Max <m.taubert2015@gmail.com>
2021-03-29 15:23:34 +0100

    players have nameplates in game

--- scenes/enviornments/multiplayer_compatible_level/NetworkPlayer.gd
+++ scenes/enviornments/multiplayer_compatible_level/NetworkPlayer.gd
@@ -1,5 +1,7 @@
 extends Spatial
+class_name NetworkPlayer
 
+var player_name:String = ''
 
 var _target_global_pos:Vector3 = Vector3.ZERO
 var _target_rotation_y:float = 0
@@ -25,8 +27,20 @@ remote func shoot() -> void:
 
 
 func _physics_process(delta:float) -> void:
+	_sync_position()
+	_scale_nameplate()
+	
+
+
+func _sync_position() -> void:
 	global_transform.origin = lerp(global_transform.origin, _target_global_pos, 0.5)
 	rotation.y = lerp_angle(rotation.y, _target_rotation_y, 0.5)
 	$GunRig.rotation.z = lerp_angle($GunRig.rotation.z, _target_rotation_z, 0.5)
 	orthonormalize()
 	$GunRig.orthonormalize()
+
+
+func _scale_nameplate() -> void:
+	var camera:Camera = get_tree().get_nodes_in_group('player_camera').front()
+	var dist:float = camera.global_transform.origin.distance_to(global_transform.origin) * 0.1
+	$NamePlate.scale = Vector3(dist, dist, dist)
