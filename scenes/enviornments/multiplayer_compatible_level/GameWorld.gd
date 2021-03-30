bf7064de7fb498c66694a7250b61e34f08a77e64
Max <m.taubert2015@gmail.com>
2021-03-30 11:46:12 +0100

    fixed host no longer spawning players

--- scenes/enviornments/multiplayer_compatible_level/GameWorld.gd
+++ scenes/enviornments/multiplayer_compatible_level/GameWorld.gd
@@ -5,16 +5,19 @@ onready var network_player:PackedScene = load("res://scenes/enviornments/multipl
 onready var local_player:KinematicBody = players.get_child(0)
 
 func _ready() -> void:
-	_move_player_to_spawn_position(local_player)
+	randomize()
 	if NetworkHelper.is_multiplayer:
 		players.get_child(0).name  = String(get_tree().get_network_unique_id()) #Sets the local player's node name to their network ID
 		_setup_multiplayer()
-		
+		if get_tree().is_network_server(): _move_player_to_spawn_position(local_player)
+		else: rpc_id(1, 'request_respawn')
+	else:
+		_move_player_to_spawn_position(local_player)
 
 
 #Network logic
 func _setup_multiplayer() -> void:
-	NetworkHelper.connect("player_joined", self, "_spawn_player")
+	NetworkHelper.connect("player_joined", self, "_spawn_network_player")
 	NetworkHelper.connect("server_disconnected", self, "_server_disconnected")
 	for player_id in NetworkHelper.player_data.keys():
 			_spawn_network_player(player_id, NetworkHelper.player_data[player_id])
@@ -24,12 +27,19 @@ func _spawn_network_player(player_id:int, player_data:Dictionary) -> void:
 	var new_player = network_player.instance()
 	new_player.name = String(player_id)
 	players.add_child(new_player)
-	_move_player_to_spawn_position(new_player)
+	if get_tree().is_network_server():
+		_move_player_to_spawn_position(new_player)
 
 
 func _move_player_to_spawn_position(player) -> void:
-	player.global_transform = $PlayerSpawn.global_transform
+	var spawn = $Spawns.get_children()[randi() % $Spawns.get_child_count()]
+	player.global_transform = spawn.global_transform
 	player.global_transform.origin.y += 2
+
+
+remote func request_respawn() -> void:
+	var player_id:int = get_tree().get_rpc_sender_id()
+	var player = players.get_node(String(player_id))
 	
-	if player is NetworkPlayer:
-		player.rpc('server_update_transform', player.global_transform)
+	_move_player_to_spawn_position(player)
+	player.rpc_id(player_id, 'server_update_transform', player.global_transform)
