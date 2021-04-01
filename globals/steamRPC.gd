b672bdf190809070ce57e8dfe33f4162bd2fffc4
bram <bramreth@gmail.com>
2021-04-01 14:04:09 +0100

    finalised shooting logic

--- globals/steamRPC.gd
+++ globals/steamRPC.gd
@@ -1,25 +1,29 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var network_players = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SteamNetwork.register_rpcs(self,
 		[
-			["shooter",  SteamNetwork.PERMISSION.CLIENT_ALL],
-			["client_shot",  SteamNetwork.PERMISSION.SERVER],
+			["client_has_shot",  SteamNetwork.PERMISSION.CLIENT_ALL],
+			["shoot_event",  SteamNetwork.PERMISSION.SERVER],
 		]
 	)
 	
-func shoot_server():
-	SteamNetwork.rpc_on_server(self, 'shooter', [])
+func tell_server_shot(rof, spread):
+	SteamNetwork.rpc_on_server(self, 'client_has_shot', [rof, spread])
 	
-func shoot_client(shooting_client):
-	SteamNetwork.rpc_all_clients(self, 'shoot', [shooting_client])
+remote func client_has_shot(shooting_client, rof, spread):
+	SteamNetwork.rpc_all_clients(self, 'shoot_event', [shooting_client, rof, spread])
 	
-remote func shooter(id):
-	shoot_client(id)
+remote func shoot_event(server_id, shooting_client, rof, spread):
+	get_or_init_client(shooting_client).shoot(rof, spread)
+
+func get_or_init_client(client_id):
+	if client_id in network_players:
+		return network_players[client_id]
+	for p in get_tree().get_nodes_in_group("network_player"):
+			if p.name == str(client_id):
+				network_players[client_id] = p
+				return p
